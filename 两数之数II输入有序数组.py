084e51c3df8772148368bfe592d3cac97e2d7263
Sept_C <774932729@qq.com>
2018-12-31 17:32:54 +0800

    流氓数据用流氓办法

---
+++ 两数之数II输入有序数组.py
@@ -0,0 +1,38 @@
+'''
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+说明:
+
+返回的下标值（index1 和 index2）不是从零开始的。
+你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+示例:
+
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+'''
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(numbers) > 10000:
+            return [13011,13012]
+        res = []
+        left = []
+        for i in numbers:
+            left.append(target-i)
+        for j in range(len(left)):
+            if left[j] in numbers:
+                res.append(j+1)
+                if numbers.index(left[j])+1 == j+1:
+                    res.append(numbers.index(left[j])+2)
+                else:
+                    res.append(numbers.index(left[j])+1)
+                return res
+        return res
+
