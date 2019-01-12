d484d3f9a831f5813bcb95490874a7e3b60987ec
Sept_C <774932729@qq.com>
2019-01-12 22:16:30 +0800

    20190112

---
+++ 旋转数组.py
@@ -0,0 +1,61 @@
+'''
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+解释: 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+说明:
+
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+要求使用空间复杂度为 O(1) 的原地算法。
+'''
+#O(kn)
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        for i in range(k):
+            temp = nums[n-1]
+            for j in range(n-1,0,-1):
+                nums[j] = nums[j-1]
+            nums[0] = temp
+#O(n)
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k % n
+        self.reverse(0,n-1,nums)
+        self.reverse(0,k-1,nums)
+        self.reverse(k,n-1,nums)
+
+    def reverse(self,start,end,array):
+        while start<end:
+            temp = array[start]
+            array[start] = array[end]
+            array[end] = temp
+            start += 1
+            end -= 1
+
+#O()
