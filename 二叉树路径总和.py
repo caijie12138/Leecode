72d7e2851add662dd70a6ca4de289eb752545f08
Sept_C <774932729@qq.com>
2019-04-28 21:55:17 +0800

    20190428

---
+++ 二叉树路径总和.py
@@ -0,0 +1,48 @@
+'''
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        res1 = 0
+
+        def getsum(root,res):
+            if root == None:
+                return None
+            res -= root.val
+            #print(res)
+            if root.left == None and root.right == None:
+                if res == 0:
+                    return True
+                else:
+                    return False
+            return getsum(root.left,res) or getsum(root.right,res)
+
+        return getsum(root,sum)
+
+
