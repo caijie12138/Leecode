ff5e49d6f8e763eb31b94807b825a8df4710ca4e
Sept_C <774932729@qq.com>
2019-05-28 09:51:49 +0800

    20190528

---
+++ 寻找重复数.py
@@ -0,0 +1,38 @@
+'''
+给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+
+示例 1:
+
+输入: [1,3,4,2,2]
+输出: 2
+示例 2:
+
+输入: [3,1,3,4,2]
+输出: 3
+说明：
+
+不能更改原数组（假设数组是只读的）。
+只能使用额外的 O(1) 的空间。
+时间复杂度小于 O(n2) 。
+数组中只有一个重复的数字，但它可能不止重复出现一次。
+
+'''
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = 0
+        fast = 0
+        while True:
+            #快慢指针 一个走两步 一个走一步
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast==slow:
+                fast = 0
+                while nums[fast]!=nums[slow]:
+                    fast = nums[fast]
+                    slow = nums[slow]
+                return nums[slow]
+
